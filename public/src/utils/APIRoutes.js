58e768aa9a7cc3f30e7e0bdae8572c62557758bc
Coder-Madaan <120481020+Coder-Madaan@users.noreply.github.com>
2024-05-16 10:16:43 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+export const host = "chat-app-original-1j3n.vercel.app
+";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
